747b714b56b55f34a021c67f9262c1711cb8c34a
defiant00 <defiant00@gmail.com>
2022-02-18 19:20:15 -0800

    ast classes

--- NSharp.Core/Ast.cs
+++ NSharp.Core/Ast.cs
@@ -1,5 +1,54 @@
 namespace NSharp.Core.Ast;
 
-public interface IExpression { }
+public interface IAstItem
+{
+    public Position Position { get; set; }
+}
 
-public interface IStatement { }
+public interface IExpression : IAstItem { }
+
+public interface IStatement : IAstItem { }
+
+public class AstItem : IAstItem
+{
+    public Position Position { get; set; }
+}
+
+public class Class : AstItem, IStatement
+{
+    public string? Name { get; set; }
+    public List<IStatement>? Statements { get; set; }
+}
+
+public class Comment : AstItem, IStatement
+{
+    public string? Content { get; set; }
+}
+
+public class CurrentObjectInstance : AstItem, IExpression { }
+
+public class ExpressionStatement : AstItem, IStatement
+{
+    public IExpression? Expression { get; set; }
+}
+
+public class File : AstItem, IStatement
+{
+    public string? Name { get; set; }
+}
+
+public class FunctionDefinition : AstItem, IStatement
+{
+    public string? Name { get; set; }
+    public List<IStatement>? Statements { get; set; }
+}
+
+public class Property : AstItem, IStatement
+{
+    public string? Name { get; set; }
+}
+
+public class Space : AstItem, IStatement
+{
+    public int Size { get; set; }
+}
---
+++ NSharp.Core/Position.cs
@@ -0,0 +1,7 @@
+namespace NSharp.Core;
+
+public struct Position
+{
+    public int Line { get; set; }
+    public int Char { get; set; }
+}
